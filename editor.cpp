b6db125ce88af52e31e7cbb1cae4a7b2789e3108
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-04 14:24:13 -0400

    Update editor.cpp
    I think Dr. Sturm recommended putting placeCursorAt ABOVE the methods, so I moved it there. Here is how I recommend implementing the constructor, but I have not done any testing.

--- editor.cpp
+++ editor.cpp
@@ -1,15 +1,5 @@
 #include "editor.h"
 
-editor::editor() {
-
-}
-void editor::DisplayLines(){
-
-}
-void editor::run() {
-
-}
-
 void placeCursorAt(Position coordinate) {
 
 	COORD coord;
@@ -27,5 +17,35 @@ void placeCursorAt(Position coordinate) {
 }
 
 
+editor::editor(string fileName) {
+	ifstream in;
+	in.open(fileName);
+	
+	if (in.fail() ) //Terminate program if input file does not open.
+	{
+		cerr << "Error. Cannot open input file. Goodbye!\n\n";
+		exit(1);
+	}	
+	string firstLine;
+	getline(in, firstLine);
+	lines.insert(1, firstLine);
+	
+	string nextLine;
+	
+	//Loop to read in each reamining line from file. Line 2 is stored at position 2, Line 3 at position 3, etc.
+	while (!eof() )
+	{
+		getline(in, nextLine);
+		lines.insert( (lines.getLength() + 1 ), nextLine);
+	}
+	
+	in.close();
+}
+void editor::DisplayLines(){
+
+}
+void editor::run() {
+
+}
 
 //void editor::
