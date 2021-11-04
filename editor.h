b097f70292fb9178ffb861565abe23ad67c3956e
kreid86 <93731916+kreid86@users.noreply.github.com>
2021-11-04 13:55:00 -0400

    Update editor.h

--- editor.h
+++ editor.h
@@ -2,12 +2,18 @@
 #define EDITOR_
 #include "LinkedList.h"
 #include<string>
+#include <ifstream>
 class editor {
 
 private:
-	LinkedList <std::string> Line;
+	LinkedList <std::string> lines;
 public:
-	void Editor();
+	/**Constructor which initializes a new editor object by reading a file into it.
+	@param fileName The name of the file to be read in.
+	@Pre the fileName denotes a valid file in the same directory.
+	@Post Terminates program if the file fileName cannot be opened. Otherwise, each line in the file becomes an item
+		in the list. */
+	Editor(string filename); //Note a constructor has no return type, not even void
 	void DisplayLines();
 	void run();
 	void placeCursorAt(Position coordinate);
